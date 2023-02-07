d03ca1dab6b14fcdc5ce1cec790ec800757afd8e
Luiz Augusto <luiz.kirk4@gmail.com>
2023-02-06 21:28:44 -0300

    adicionado numero ultizado anteriormente na conta acima do numero atual da tela e a operação realizada em ambos

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-// seleciona o corpo da calc
 const calculadora = document.querySelector('.corpo');
-//seleciona a span onde fica o resultado
 const resultado = document.querySelector('.resultado .calculo');
-//seleciona todos os botoes
 const btns = calculadora.querySelector('.botoes');
-// adicionamos o evento de clique para todos os botoes
+const nmrAnterior = document.querySelector('.anterior');
+
 btns.addEventListener('click', (tecla) => {
-  // seleciona o botao especiofico
   const btnEspecifico = tecla.target;
-  // texto do atributo dataset de cada botao
   const operacao = btnEspecifico.dataset.acao;
-  // conteudo do botao especifico
   const btnEspecificoConteudo = btnEspecifico.innerText;
-  // captura o conteudo atual no span
   const nmrAtualNaTela = resultado.innerText;
-  // se for um botao remova a classe "apertado dele
+
   if (btnEspecifico.matches('button')) {
     Array.from(btnEspecifico.parentElement.children).forEach((botao) => {
       botao.classList.remove('apertado');
     });
   }
-  // se nao for uma operacao matematica
   if (!operacao){
     if (nmrAtualNaTela === '0'){
-      // muda o numero 0 para o primeiro botao clicado
-      resultado.innerText = btnEspecificoConteudo;
+       resultado.innerText = btnEspecificoConteudo;
     } else {
-      // se nao for 0, contate com os numero existente
       resultado.innerText = nmrAtualNaTela + btnEspecificoConteudo;
     }
   }
@@ -34,35 +25,30 @@ btns.addEventListener('click', (tecla) => {
   operacao === 'subtrai' ||
   operacao === 'multiplica' ||
   operacao === 'divide'){
-    // somente para adicionar um estilo no operador selecionado o diferenciando dos demais
     btnEspecifico.classList.add('apertado');
-    // cria um atributo que captura o primeiro valor
     calculadora.dataset.firstValue = nmrAtualNaTela;
-    // cria um atributo que captura o valor do operador selecionado
     calculadora.dataset.digito = operacao;
-    // altera o conteudo atual para o novo inserido apos clicar em um operador
+    nmrAnterior.innerText = nmrAtualNaTela;
     resultado.innerText = "";
   }
-  // só insere o ponto para ser usado em numeros flutuantes
     if (operacao === 'ponto'){
         resultado.innerHTML = nmrAtualNaTela + '.';
       }
-      // retorna o primeiro valor para 0
     if (operacao === 'limpa'){
+      nmrAnterior.innerText = '0';
       resultado.innerHTML = '0';
       }
-      // caso aperte em calcular
     if (operacao === 'calcula') {
       const primeiroValor = calculadora.dataset.firstValue;
       const operador = calculadora.dataset.digito;
       const segundoValor = nmrAtualNaTela;
-      // chamamos a funcao que calcula e colocamos o retorno dela no resultado
       resultado.innerText = calcular(primeiroValor, operador, segundoValor);
+      mostraAnterior(primeiroValor, operador, segundoValor);
+      
       }
       function calcular(n1, operador, n2) {
         let retorno;
         if (operador === 'soma') {
-          //parseFloat para retornar um numero flutuante
           retorno = parseFloat(n1) + parseFloat(n2);
         } else if (operador === 'subtrai') {
           retorno = parseFloat(n1) - parseFloat(n2);
@@ -73,6 +59,19 @@ btns.addEventListener('click', (tecla) => {
         }
         return retorno;
       }
-      
-    }
+      function mostraAnterior(n1, operador, n2){
+        if (operador === 'soma'){
+          nmrAnterior.innerText = n1+' + '+n2;
+        }
+        if (operador === 'subtrai'){
+          nmrAnterior.innerText = n1+' - '+n2;
+        }
+        if (operador === 'multiplica'){
+          nmrAnterior.innerText = n1+' * '+n2;
+        }
+        if (operador === 'divide'){
+          nmrAnterior.innerText = n1+' / '+n2;
+        }
+      }
+  }
 )
